a01e2465ddabe4195ed0d7d2fc4e29d3f2156b2e
ILostAccessToYcGun <ycgun32@gmail.com>
2024-11-24 11:42:58 -0500

    Fixed up the holding mechanic so now, strafing and moving the object is very smooth and matches the player camera. Unfortunately this removes the yeet feature but its okay.

--- Assets/Scripts/PlayerControls/PlayerPickup.cs
+++ Assets/Scripts/PlayerControls/PlayerPickup.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,7 @@ public class PlayerPickup : MonoBehaviour
     PlayerInput input;
     InputAction pickUpAction;
 
-    public GameObject holdPoint;
+    public Transform holdPoint;
     [SerializeField] GameObject currentObjectHeld;
 
     public float pickUpRange;
@@ -19,6 +20,9 @@ public class PlayerPickup : MonoBehaviour
 
     float pickUpTimer;
     public float pickUpCooldown;
+
+
+    
 
     //:O i wake
     void Awake()
@@ -39,7 +43,6 @@ public class PlayerPickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(pickUpTimer);
         if (pickedUp)
         {
             Vector3 direction = (holdPoint.transform.position - currentObjectHeld.transform.position);
@@ -53,71 +56,17 @@ public class PlayerPickup : MonoBehaviour
             if (objectVelocityDirection.x > 0 && targetDirection.x > 0 || objectVelocityDirection.x < 0 && targetDirection.x < 0 ||
                 objectVelocityDirection.y > 0 && targetDirection.y > 0 || objectVelocityDirection.y < 0 && targetDirection.y < 0 ||
                 objectVelocityDirection.z > 0 && targetDirection.z > 0 || objectVelocityDirection.z < 0 && targetDirection.z < 0)
-            {
-
-                //tried reverse
-                //if (!(objectVelocityDirection.x > 0 && targetDirection.x > 0) || !(objectVelocityDirection.x < 0 && targetDirection.x < 0) ||
-                //    !(objectVelocityDirection.y > 0 && targetDirection.y > 0) || !(objectVelocityDirection.y < 0 && targetDirection.y < 0) ||
-                //    !(objectVelocityDirection.z > 0 && targetDirection.z > 0) || !(objectVelocityDirection.z < 0 && targetDirection.z < 0))
-                //{
-                //      acceleration *= 2;
-
-
-
+            
                 //Okay so currently, With this system, the object feels very smooth to move around and you can throw it,
                 //but If you strafe or move without moving the mouse, the object will be very jittery. This will work for a Demo,
                 //but will need to be adjusted for a 3D half build
                 hit.rigidbody.velocity *= 0.98f;
-
-
-
-                //tried to use derrivative here
-                //acceleration = new Vector3((0.98f / 2) * hit.rigidbody.velocity.x * hit.rigidbody.velocity.x,
-                //    (0.98f / 2) * hit.rigidbody.velocity.y * hit.rigidbody.velocity.y,
-                //    (0.98f / 2) * hit.rigidbody.velocity.z * hit.rigidbody.velocity.z);
-
-                //float decel = 0.2f;
-
-                //if (Mathf.Abs(hit.rigidbody.velocity.x) > 0 )
-                //{
-                //    acceleration.x -= decel;
-
-                //}
-                //if (Mathf.Abs(hit.rigidbody.velocity.y) > 0)
-                //{
-                //    acceleration.y -= decel;
-
-                //}
-                //if (Mathf.Abs(hit.rigidbody.velocity.z) > 0)
-                //{
-                //    acceleration.z -=  decel;
-
-                //}
-                //acceleration = direction * 0.01f;
-                //if (direction.magnitude < 1f)
-                //{
-                //    acceleration *= -0.01f;
-                //}
-
-
-
-                //hit.rigidbody.velocity = new Vector3(Mathf.Lerp(hit.rigidbody.velocity.x, 0, Time.deltaTime * 5), Mathf.Lerp(hit.rigidbody.velocity.y, 0, Time.deltaTime * 5), Mathf.Lerp(hit.rigidbody.velocity.z, 0, Time.deltaTime * 5));
-
-            }
-
-            //slow, scaling speed that doesnt feel magnetic, more like sliding
-            //hit.rigidbody.velocity = direction * 0.2f;
-
+            
             if (direction.magnitude != 0)
-            {
                 hit.rigidbody.velocity = hit.rigidbody.velocity + acceleration;
-            }
-            //currentObjectHeld.transform.position = holdPoint.transform.position;
         }
         if (pickUpTimer > 0)
-        {
             pickUpTimer = pickUpTimer - Time.deltaTime;
-        }
 
 
 
@@ -132,36 +81,53 @@ public class PlayerPickup : MonoBehaviour
 
     private void OnBeforeMove()
     {
-        // set up raycast
-        Ray ray = new Ray(player.cam.transform.position, player.cam.transform.forward);
-      
         if (pickUpTimer <= 0)
         {
             if (pickUpAction.IsPressed()) //if the player is pressing the button to hold:
             {
+                Debug.Log("huh");
+                // set up raycast
+                Ray ray = new Ray(player.cam.transform.position, player.cam.transform.forward);
                 if (Physics.Raycast(ray, out hit, pickUpRange, holdMasks)) //and we hit something in range in the correct layer
                 {
+                    
                     if (pickedUp) //if we already have object picked up
                     {
-                        currentObjectHeld = null; //stop picking it up
-                        pickedUp = false;
+                        Debug.Log("ruh");
+                        DropObject();
                     }
+                        
                     else //if we dont currently have an object picked up
                     {
-                        currentObjectHeld = hit.collider.gameObject; //make the hit object, the held object
-                        pickedUp = true;
+                        Debug.Log("roh");
+                        HoldObject();
                     }
                     pickUpTimer = pickUpCooldown;
                 }
                 else
                 {
-                    //im annoyed that I have to do this twice
-                    currentObjectHeld = null; //stop picking it up
-                    pickedUp = false;
+                    Debug.Log("raggy");
+                    DropObject();
                 }
-                
-                
             }
         } 
     }
+
+    void HoldObject()
+    {
+        hit.collider.gameObject.transform.parent = holdPoint;
+        currentObjectHeld = hit.collider.gameObject; //make the hit object, the held object
+        pickedUp = true;
+    }
+
+    void DropObject()
+    {
+        
+        currentObjectHeld.transform.parent = null;
+        currentObjectHeld = null; //stop picking it up
+        pickedUp = false;
+    }
+
 }
+
+
